8ac694192c92a6f65337ed2e8d88ddb7debe6a78
… <thomasquint8@gmail.com>
2021-04-25 10:03:09 -0300

    primeros pasos

--- src/App.js
+++ src/App.js
@@ -5,21 +5,14 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+                <a href="#home">INICIO</a>
+                <a href="#productos">PRODUCTOS</a>
+                <a href="#novedades">NOVEDADES</a>
+                <a href="#contacto">CONTACTO</a>   
       </header>
     </div>
-  );
+    );
 }
 
+ 
 export default App;
